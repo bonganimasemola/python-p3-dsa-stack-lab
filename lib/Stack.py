286c163dc3c96c797e028ca8e665f24c5a782c2f
bonganimasemola <bonganimasemola@icloud.com>
2023-12-08 20:55:36 +0300

    70% done, remaining two tests are blockers

--- lib/Stack.py
+++ lib/Stack.py
@@ -1,13 +1,29 @@
 class Stack:
 
     def __init__(self, items = [], limit = 100):
-        pass
-
+        self.items = items
+        self.limit = limit
+        
+        # initialize self.dictionary with a default value for each index in the range of limit, if limit is meant to be the maximum number of items that the stack can hold
+        self.dictionary = {i: None for i in range(limit)}
+    
     def isEmpty(self):
-        pass
+        return self.items == []
+                
 
     def push(self, item):
-        pass
+        # This method adds a new element to the top of the stack.
+        # It first checks if the stack is full by comparing the length of the items list with the limit.
+        # If the stack is full, it raises an exception.
+        # Otherwise, it adds the item to the items list and the corresponding index to the dictionary.
+        # Returns the updated stack.
+        if len(self.items) == self.limit:
+            raise Exception("Stack is full.")
+        # interestingly if you just input the below the Push to 0 & Pop 1 off the stack tests pass pytest 
+        self.dictionary[item] = None
+        self.items.append(item)
+        return self 
+
 
     def pop(self):
         pass
@@ -15,11 +31,16 @@ class Stack:
     def peek(self):
         pass
     
-    def size(self):
-        pass
-
+    def size(self) -> int:
+        return len(self.items)
+    
+        
     def full(self):
-        pass
+        return len(self.items) == self.limit
+        
 
     def search(self, target):
-        pass
+        for i in reversed(range(len(self.items))):
+            if self.items[i] == target:
+                return len(self.items) -1  - i 
+        return -1
